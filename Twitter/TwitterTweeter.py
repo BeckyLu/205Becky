29644b9e7785ebc194f7790d0a885b08c2624efc
BeckyCookiez <cookiez0rz321@gmail.com>
2015-04-03 20:49:58 +0100

    Fixes

--- Twitter/TwitterTweeter.py
+++ Twitter/TwitterTweeter.py
@@ -1,13 +1,11 @@
-import twitter, datetime
-google = sqlite3.connect(
+import twitter, datetime, sqlite3
+google = sqlite3.connect("C:\Users\Steve\AppData\Local\Google\Chrome\User Data\Default\History")
 cursor = google.cursor()
-
-google = sqlite3.connect("C:\Users\Robert\AppData\Local\Google\Chrome\User Data\Default\History")
 
 cursor = google.cursor()
 cursor.execute("SELECT * FROM urls")
 rows = cursor.fetchall()
-data = " "
+#data = " "
 for line in rows :
     global data 
     data = line[1]
@@ -27,7 +25,7 @@ consumer_secret="KX2om4lFxKNTBm7Lsve9g3enLBjl57nCmGPqSD47ZWzn8TJsIf",
 access_token_key="1898676804-1SFQSCITWTfI49crXNwP6vuUEoBIWZrcXuUNUar",
 access_token_secret="g0FguJruvzD3hAmwd6yTdHe4xGFrJN5jTCX05EppXCpTf")
 
-print api.VerifyCredentials()
+#print api.VerifyCredentials()
 
 statuses = api.GetUserTimeline(user)
 
@@ -35,6 +33,10 @@ timestamp = datetime.datetime.utcnow()
 
 response = api.PostUpdate("This is being Tweeted from a python sketch. Tweeted at " + str(timestamp))
 
+lastWebsite = api.PostUpdate("The last website I Visited was: " + data)
+
 print("Status updated to: " + response.text)
+
+print("Status updated to: " + lastWebsite.text)
 
 print (statuses[0].text)
