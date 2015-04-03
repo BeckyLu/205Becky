416a33656e9733113988141f5d8fbc4ef95cc887
BeckyLu <Beckylselby@gmail.com>
2015-04-03 23:04:42 +0100

    Chatbot changes

--- ChatBot/ChatBotTemplate.py
+++ ChatBot/ChatBotTemplate.py
@@ -1,28 +1,32 @@
-# Knights to was "ni" video reference
-# https://www.youtube.com/watch?v=zIV4poUZAQo
+import urllib
+import wikipedia
+import bs4
 
 file = open("stop-words.txt")
 stopwords = file.readlines()
 
-for word in stopwords:
-    next = word.strip()
+#for word in stopwords:
+#    next = word.strip()
     #print(next)
 
-def removeStopwords(removeWord):
-    
+def removeStopwords(removeWord):  
     for stopword in stopwords:
-        removeWord = removeWord.replace(" " + stopword.strip() + " ", " ")
+        message = stopword.strip()
+        removeWord = removeWord.replace(" " + message + " ", " ")
         #print removeWord        #Debugging
     return removeWord
     
-while True:
-    inputString = raw_input("Greetings! Who are you? ")
-    filtered = removeStopwords(inputString)
+
+inputString = raw_input("Greetings! Who are you? ")
+inputString = " " + inputString + " "
+name = removeStopwords(inputString)
+print("Hello " + name.strip() + " How're you today?")
+    #filtered = removeStopwords(inputString)
     
     #print filtered            #Debugging
     #print inputString         #Debugging
     
-    print("Hello there " + filtered + "How're you today?")
+    #print("Hello there " + filtered + " How're you today?")
 #else:
     
     
