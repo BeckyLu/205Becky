3ab9070a0972d95bd91fc999c3e40a57fba25999
BeckyCookiez <cookiez0rz321@gmail.com>
2015-04-03 15:57:25 +0100

    Laptop changes
    Twitter

--- Twitter/TwitterTweeter.py
+++ Twitter/TwitterTweeter.py
@@ -10,11 +10,12 @@ cred = file.readline().strip().split(',')
 
 api = twitter.Api(
 
-consumer_key=cred[1],
-consumer_secret=cred[2],
-access_token_key=cred[3],
-access_token_secret=cred[4])
+consumer_key="5HlfJs6N70i0N9y0z2nmy1MOV",
+consumer_secret="KX2om4lFxKNTBm7Lsve9g3enLBjl57nCmGPqSD47ZWzn8TJsIf",
+access_token_key="1898676804-sZlf8R5VmAourSLpPiCPqcich7Etp7Wz8K4XjUZ",
+access_token_secret="hlI9YiDk5rSLHTToE1zt9wzj59S1PlhMpK8XuEL1l9lpX")
 
+print api.VerifyCredentials()
 
 statuses = api.GetUserTimeline(user)
 
