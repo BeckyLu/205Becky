3bbc0745c37059c3cd192432773f301a6b819042
BeckyLu <Beckylselby@gmail.com>
2015-04-03 23:45:16 +0100

    Fixes2

--- ChatBot/ChatBotTemplate.py
+++ ChatBot/ChatBotTemplate.py
@@ -5,28 +5,26 @@ import bs4
 file = open("stop-words.txt")
 stopwords = file.readlines()
 
-#for word in stopwords:
-#    next = word.strip()
-    #print(next)
+for word in stopwords:
+    next = word.strip()
+    print(next)
 
-def removeStopwords(removeWord):  
+def removeStopwords(removeWord):
+    
     for stopword in stopwords:
-        message = stopword.strip()
-        removeWord = removeWord.replace(" " + message + " ", " ")
+        removeWord = removeWord.replace(" " + stopword.strip() + " ", " ")
         #print removeWord        #Debugging
     return removeWord
     
 
-inputString = raw_input("Greetings! Who are you? ")
-inputString = " " + inputString + " "
-name = removeStopwords(inputString)
-print("Hello " + name.strip() + " How're you today?")
-    #filtered = removeStopwords(inputString)
+while True:
+    inputString = raw_input("Greetings! Who are you? ")
+    filtered = removeStopwords(inputString)
     
-    #print filtered            #Debugging
-    #print inputString         #Debugging
+    print filtered            #Debugging
+    print inputString         #Debugging
     
-    #print("Hello there " + filtered + " How're you today?")
+    print("Hello there " + filtered)
 #else:
     
     
---
+++ ChatBot/Run Chatbot.bat
@@ -0,0 +1,2 @@
+python chatbottemplate.py
+pause
